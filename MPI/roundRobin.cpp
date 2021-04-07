a3ddc9931411d427b8493261dfee4527ca8572c6
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-04-07 18:30:49 +0700

    add simultaneously sendrecv method

--- MPI/roundRobin.cpp
+++ MPI/roundRobin.cpp
@@ -36,6 +36,27 @@ void roundRobin(int rank, int numProcs) {
 
 }
 
+void roundRobinSinultaneouslySendRecv(int rank, int numProcs) {
+	const int MAX_RAND = 20;
+	long int mineRand = 0, prevRand = 0;
+	int nextRank = (rank + 1) % numProcs;
+	int prevRank = rank == 0 ? numProcs - 1 : rank - 1;
+	MPI_Status status;
+
+	std::srand(std::time(nullptr) + rank);
+	mineRand = std::rand() % MAX_RAND;
+
+	std::cout << rank << ": random is " << mineRand << std::endl;
+
+	MPI_Sendrecv(static_cast<void*>(&mineRand), 1, MPI_LONG, nextRank, 1,
+		static_cast<void*>(&prevRand), 1, MPI_LONG, prevRank, 1,
+		MPI_COMM_WORLD, &status
+	);
+
+	std::cout << rank << ": >> I had " << mineRand << ", " << prevRank << " had " << prevRand << std::endl;
+
+}
+
 int main(int argc, char** argv) {
 	int numProcs;
 	int rank;
@@ -47,7 +68,8 @@ int main(int argc, char** argv) {
 	assert(numProcs % 2 == 0 && "There cant be odd amount of processes");
 
 	std::cout << rank << ": hello (p = " << numProcs << ")" << std::endl;
-	roundRobin(rank, numProcs);
+	//roundRobin(rank, numProcs);
+	roundRobinSinultaneouslySendRecv(rank, numProcs);
 	std::cout << rank << ": goodbye" << std::endl;
 
 	MPI_Finalize();
