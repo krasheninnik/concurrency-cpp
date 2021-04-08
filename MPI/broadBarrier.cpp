383943cc3f9a299657cc5ce500fd259c7e9cda5e
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-04-08 10:32:44 +0700

    implement barrier and broadcast

--- MPI/broadBarrier.cpp
+++ MPI/broadBarrier.cpp
@@ -5,12 +5,44 @@
 #include <cassert>
 #include <cstdarg>
 #include "logger/owl.hpp"
+#include <chrono>
+#include <thread>
 
 // run: mpiexec -n 4 projectName.exe
-void roundRobin(int rank, int numProcs) {
+int broadcast(int rank, int numProcs) {
+	const int MAX_RAND = 20;
+	long randValue = 0;
+	int bradcasterRank = numProcs - 1;
 
+	if (rank == bradcasterRank) {
+		std::srand(std::time(nullptr) + rank);
+		randValue = std::rand() % MAX_RAND;
+		owl << rank << ": broadcasting " << randValue << std::endl;
+	}
+
+	MPI_Bcast(static_cast<void*>(&randValue),
+		1, MPI_LONG,
+		bradcasterRank,
+		MPI_COMM_WORLD);
+
+	if (rank != bradcasterRank) {
+		owl << rank << ": received " << randValue << std::endl;
+	}
+
+	return randValue;
 }
 
+void barrier(int rank, int numProcs) {
+	std::srand(std::time(nullptr) + rank);
+	int napTime = std::rand() + rank*1000;
+	owl << rank << ": sleeping " << napTime << std::endl;
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(napTime));
+
+	owl << rank << ": enter barrier" << std::endl;
+	MPI_Barrier(MPI_COMM_WORLD);
+	owl << rank << ": leave barrier" << std::endl;
+}
 
 int main(int argc, char** argv) {
 
@@ -20,7 +52,6 @@ int main(int argc, char** argv) {
 	settings.b_stdout = true;
 	settings.b_fileline = false;
 	OwlInit(settings);
-	owl << "check this line" << std::endl;
 
 	int numProcs;
 	int rank;
@@ -29,12 +60,10 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	//assert(numProcs % 2 == 0 && "There cant be odd amount of processes");
-
-	std::cout << rank << ": hello (p = " << numProcs << ")" << std::endl;
-	//roundRobin(rank, numProcs);
-	//roundRobin(rank, numProcs);
-	std::cout << rank << ": goodbye" << std::endl;
+	owl << rank << " hello (p = " << numProcs << ")" << std::endl;
+	auto randomValue = broadcast(rank, numProcs);
+	barrier(rank, randomValue);
+	owl << rank << " goodbye" << std::endl;
 
 	MPI_Finalize();
 }
